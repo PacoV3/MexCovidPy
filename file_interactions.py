d50f926c8ae140860c86f17610b1b2659db6e9b5
PacoV3 <franj.vents@gmail.com>
2021-07-21 22:01:40 -0500

    Changes in structure in main and file_interactions

--- file_interactions.py
+++ file_interactions.py
@@ -1,34 +1,50 @@
 from zipfile import ZipFile
+from csv import DictReader
 import requests
 import os
 
 
 class FileInteractions:
-    def __init__(self, zip_path, folder_path):
+    def __init__(self, zip_path, folder_path, url):
         self.zip_path = zip_path
         self.folder_path = folder_path
+        self.url = url
 
 
-    def download_url(self, url, chunk_size=128):
-        r = requests.get(url, stream=True)
+    def download_url(self, chunk_size=128):
+        r = requests.get(self.url, stream=True)
+        if not os.path.exists(self.folder_path):
+            os.makedirs(self.folder_path)
         with open(self.zip_path, 'wb') as fd:
             for chunk in r.iter_content(chunk_size=chunk_size):
                 fd.write(chunk)
 
 
     def extract_csv(self):
+        file_name = ''
         with ZipFile(self.zip_path,'r') as myzip:
+            file_name = myzip.namelist()[0]
             myzip.extractall(self.folder_path)
         os.remove(self.zip_path)
+        return file_name
 
 
-    def get_csv_date(self):
-        year = month = day = ''
-        files = os.listdir(self.folder_path)
-        if len(files) == 1:
-            file_name, ext = os.path.splitext(files[0])
-            if ext == '.csv' and file_name:
-                year = file_name[:2]
-                month = file_name[2:4]
-                day = file_name[4:6]
-        return year, month, day
+    def get_csv_date(self, file_name):
+        from datetime import datetime
+        date_vals = file_name[:6]
+        return datetime.strptime(date_vals, '%y%m%d')
+
+    def get_indicators(self, cities, file_name):
+        with open(f'{self.folder_path}/{file_name}', 'r', encoding="ISO-8859-1") as csv_f:
+            f = DictReader(csv_f)
+            for row in f:
+                city_index = (int(row['MUNICIPIO_RES']), int(row['ENTIDAD_RES']))
+                if city_index in cities:
+                    if int(row['RESULTADO']) == 1:
+                        cities[city_index]['confirmed'] += 1
+        return cities
+
+    
+    def confirmed(self, row, date):
+        if int(row['RESULTADO']) == 1:
+            pass            
--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 from file_interactions import FileInteractions
+from sqlalchemy.orm import sessionmaker
+from models import City, DbInteraction
+from sqlalchemy import select
 
 
 def main():
-    file_interact = FileInteractions('zip/file.zip', 'zip')
-    # # Specific 2020 day
+    # Specific 2020 day
     url = 'http://datosabiertos.salud.gob.mx/gobmx/salud/datos_abiertos/historicos/09/datos_abiertos_covid19_12.09.2020.zip'
-    # # Specific 2021 day
+    # Specific 2021 day
     # url = 'http://datosabiertos.salud.gob.mx/gobmx/salud/datos_abiertos/historicos/2021/04/datos_abiertos_covid19_09.04.2021.zip'
     # Last day
     # url = 'http://datosabiertos.salud.gob.mx/gobmx/salud/datos_abiertos/datos_abiertos_covid19.zip'
-    file_interact.download_url(url)
-    file_interact.extract_csv()
-    year, month, day = file_interact.get_csv_date()
-    print(year, month, day)
+    file_interact = FileInteractions('zip/file.zip', 'zip', url)
+    file_interact.download_url()
+    csv_name = file_interact.extract_csv()
+    # csv_date = file_interact.get_csv_date(csv_name)
+
+    # Indicators test
+    db = DbInteraction('localhost,5432', 'mex_covid_py',
+                       'postgres', 'this_isAstrong#password')
+    Session = sessionmaker(bind=db.engine)
+    ses = Session()
+    cities = {}
+
+    result = ses.execute(select(City)).scalars().all()
+    for city in result:
+        cities[(city.city_code, city.state_id)] = {'confirmed': 0, 'city_id': city.id}
+    daily_indicators = file_interact.get_indicators(cities, csv_name)
+    for city_summary in daily_indicators.values():
+        print(city_summary)
+
 
 
 if __name__ == '__main__':
